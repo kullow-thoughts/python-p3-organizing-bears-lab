c5914135a35fc424f32cfc4ab71f30620216556d
Abdulhakimkullow <abdulhakim.kullow@student.moringaschool.com>
2024-06-05 17:34:59 +0300

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Mr. Chocolate',9,'M','black','Melancholic',true);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Rowdy',10,'M','white','Choleric',true);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Tabitha',7,'F','white','Sanguine',true);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Sergeant Brown',3,'M','brown','Choleric',true);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Melissa',4,'F','black','Sanguine',true);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Grinch',2,'F','black','Phlegmatic',true);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Wendy',4,'F','brown','Phlegmatic',true);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES(NULL,5,'M','white','Melancholic',false);
